666ec2c8b19da8f65a58ab3d3dd3cab5e6376461
PSLCOL\jmoreno <jmoreno@psl.com.co>
2017-12-14 06:47:07 -0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#light-slider").lightSlider({
+        item: 5,
+        slideMargin: 5
+    });
+
+    $("#light-slider2").lightSlider({
+        item: 8,
+        slideMargin: 5
+    });
+});
