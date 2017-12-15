f2366bcab7bc684298742d50b90381f108cf26cd
PSLCOL\jmoreno <jmoreno@psl.com.co>
2017-12-15 11:28:00 -0500

    tag cloud

--- js/custom.js
+++ js/custom.js
@@ -12,4 +12,21 @@ $(document).ready(function() {
         item: 2,
         slideMargin: 0
     });
+
+    var words = [
+        {text: "DevOps", weight: 13, link: 'http://github.com/mistic100/jQCloud'},
+        {text: "Ux", weight: 10.5, link: 'http://www.strangeplanet.fr'},
+        {text: "Innovation", weight: 9.4, link: 'http://piwigo.org'},
+        {text: "Java", weight: 13, link: 'http://github.com/mistic100/jQCloud'},
+        {text: "Spark", weight: 10.5, link: 'http://www.strangeplanet.fr'},
+        {text: "Big Data", weight: 9.4, link: 'http://piwigo.org'},
+        {text: "NodeJs", weight: 13, link: 'http://github.com/mistic100/jQCloud'},
+        {text: "Docker", weight: 10.5, link: 'http://www.strangeplanet.fr'},
+        {text: ".Net", weight: 9.4, link: 'http://piwigo.org'}
+        
+      ];
+    $('#keywords').jQCloud(words, {
+        width: 1900,
+        height: 500
+    });
 });
